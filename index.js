58299f365d7b1aa20caec39e93d86a1c8155b47f
Elizabeth <elizabethcommon12@gmail.com>
2025-09-04 22:16:07 -0400

    computer function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+
+function getComputerChoice(){
+    let choice = ['rock', 'paper', 'scissors'];
+    let index = Math.floor(Math.random() * choice.length);
+    return choice[index];
+}
+
+let word = getComputerChoice();
+console.log (word);
+
+
