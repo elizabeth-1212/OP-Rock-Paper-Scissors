420786c36b3a838fb623404b659e73c59e4024d8
Elizabeth <elizabethcommon12@gmail.com>
2025-09-07 17:48:31 -0400

    adding to code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function getComputerChoice(){
+function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         return "rock";
@@ -11,24 +11,40 @@ function getComputerChoice(){
 }
 
 
-function playRound (humanChoice, compChoice) {
-    
-    if (humanChoice === compChoice) {
-        return "It's a tie!";
-    } 
-    
-    else if (
-        (humanChoice === "rock" && compChoice === "scissors") ||
-        (humanChoice === "paper" && compChoice === "rock") ||
-        (humanChoice === "scissors" && compChoice === "paper") 
-    ){
-        return `You win, ${humanChoice} beats ${compChoice}.`;
-    }
-    return `You lose, ${compChoice} beats ${humanChoice}.`;
+function getHumanChoice() {
+    let choice = prompt("Choose your weapon: rock, paper, or scissors.");
+    return choice;
 }
 
 
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
+}
 
+function playRound (humanChoice, computerChoice) {
+    //humanChoice = humanChoice.toLowerCase();
+    
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! You both chose ${humanChoice}.`);
+    } 
+    
+    else if 
+        (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper") { 
+        
+        console.log(`You win, ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+
+    else {
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+}
+
+/*
 // Get the computer's choice
 const computerSelection = getComputerChoice();
 
@@ -38,5 +54,6 @@ const playerSelection = prompt('Choose your weapon: rock, paper, or scissors?');
 // Check the result and display it
 const result = playRound(playerSelection, computerSelection);
 alert(`Computer chose: ${computerSelection}\nYour choice: ${playerSelection}\n\n${result}`);
+*/
 
 
