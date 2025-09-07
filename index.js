76a997477cb9917719cd8b80a0a4b2e5a197417d
Elizabeth <elizabethcommon12@gmail.com>
2025-09-07 16:53:40 -0400

    reworking code

--- index.js
+++ index.js
@@ -10,60 +10,33 @@ function getComputerChoice(){
     }
 }
 
-/*
-function getHumanChoice(){
-    let humanChoice = prompt("Please choose: rock, paper, or scissors.").toLowerCase();
 
-    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-        alert ("Invalid choice.");
-        humanChoice = prompt("Please enter: rock, paper, or scissors.").toLowerCase();
-    }
-    return humanChoice;
-} 
-console.log (getHumanChoice);
-*/
-
-function playRound (compChoice, humanChoice) {
-    if (compChoice === humanChoice) {
-        return "It's a tie.";
-    }
-
+function playRound (humanChoice, compChoice) {
+    
+    if (humanChoice === compChoice) {
+        return "It's a tie!";
+    } 
+    
     else if (
         (humanChoice === "rock" && compChoice === "scissors") ||
-        (humanChoice === "scissors" && compChoice === "paper") ||
-        (humanChoice === "paper" && compChoice === "rock")
+        (humanChoice === "paper" && compChoice === "rock") ||
+        (humanChoice === "scissors" && compChoice === "paper") 
     ){
-        return `You win, ${humanChoice} beats ${compChoice}.`; 
+        return `You win, ${humanChoice} beats ${compChoice}.`;
     }
-
     return `You lose, ${compChoice} beats ${humanChoice}.`;
 }
 
-const getComputerChoice = compChoice();
-const getHumanChoice = humanChoice();
 
-
-/*
-function getHumanChoice(){
-    let humanChoice = prompt("Please choose: rock, paper, or scissors.").toLowerCase();
-
-    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-        alert ("Invalid choice.");
-        humanChoice = prompt("Please enter: rock, paper, or scissors.").toLowerCase();
-    }
-    return humanChoice;
-} 
-console.log (getHumanChoice);
-*/
 
 // Get the computer's choice
-const computerChoice = getComputerChoice();
+const computerSelection = getComputerChoice();
 
 // Prompt the player for their choice
-const playerChoice = prompt('Choose your weapon: rock, paper, or scissors?');
+const playerSelection = prompt('Choose your weapon: rock, paper, or scissors?');
 
 // Check the result and display it
-const result = playRound(playerChoice, computerChoice);
-alert(`Computer chose: ${computerChoice}\nYour choice: ${playerChoice}\n\n${result}`);
+const result = playRound(playerSelection, computerSelection);
+alert(`Computer chose: ${computerSelection}\nYour choice: ${playerSelection}\n\n${result}`);
 
 
